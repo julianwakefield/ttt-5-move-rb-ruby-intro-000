968ef1272d9db7ea4347542bd164457542610d06
Julian Wakefield <julianwakefield@outlook.com>
2019-09-18 21:11:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+board = [" ", " ", " "]
+def input_to_index(input)
+  input.to_i - 1
+
+end
+
+def move(board, [0, 1 , 2, 3, 4, 5, 6, 7, 8], X)
+  move = ["X", "0"]
+  current_player = X
+end
